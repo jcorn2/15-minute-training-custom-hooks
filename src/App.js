7a741e92cf2931d6f2da7a13af68c94fd9463b18
James Cornelius <jcorn2@umbc.edu>
2020-05-02 16:14:35 -0400

    Switch to plain create react app

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import React from 'react';
-import useChromeStorage from './useChromeStorage';
+import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  const { myName } = useChromeStorage('myName');
-
   return (
     <div className="App">
-      <p>Hi my name is {myName}</p>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
